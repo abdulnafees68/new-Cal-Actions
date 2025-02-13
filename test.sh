524539f8bb388170d31351c43469834b865b75af
Abdul Nafees <abdulnafees@Abduls-MacBook-Pro.local>
2025-02-12 23:13:58 -0700

    final commit

--- test.sh
+++ test.sh
@@ -1,1 +1,29 @@
-aiaaa
+#!/bin/bash
+
+# Simple Calculator Script using command-line arguments
+
+num1=$1
+num2=$2
+op=$3
+
+case $op in
+  +)
+    echo "Result: $(($num1 + $num2))"
+    ;;
+  -)
+    echo "Result: $(($num1 - $num2))"
+    ;;
+  \*)
+    echo "Result: $(($num1 * $num2))"
+    ;;
+  /)
+    if [ "$num2" -ne 0 ]; then
+      echo "Result: $(($num1 / $num2))"
+    else
+      echo "Error: Division by zero is not allowed."
+    ;fi
+    ;;
+  *)
+    echo "Invalid operation"
+    ;;
+esac
